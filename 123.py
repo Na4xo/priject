ca4ac6825c0cf97ae75321a4e0531896087d8938
Na4xo <davidsatel033@gmail.com>
2023-12-03 23:45:47 +0300

    Update 123.py

--- 123.py
+++ 123.py
@@ -34,8 +34,8 @@ def logout():
     for key, value in users.items():
         if value == token:
             del users[key]
-            return 'крутаааа вышел'
-    return 'ощепка'
+            return 'Пользователь вышел из чата'
+    return 'Ошибка'
 
 
 @app.route("/send")
@@ -53,7 +53,7 @@ def send():
        if value == token:
             return text
      
-    return 'err'
+    return 'Ошибка'
    
 
 @app.route("/getall")
@@ -65,5 +65,5 @@ def getall():
 
 
 if __name__ == "__main__":
-    print('RABOTAEM')
+    print('Работает')
     {app.run(debug=True)}
