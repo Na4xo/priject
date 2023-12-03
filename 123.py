55d0bf58937b9e15c5ad8583be96a3cc0d13b6fa
Na4xo <davidsatel033@gmail.com>
2023-12-03 19:15:17 +0300

    Create 123.py

---
+++ 123.py
@@ -0,0 +1,69 @@
+
+from flask import Flask, Response , request
+from flask_cors import CORS
+import uuid
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+# /method?argumen1=value1&argumen2=value2
+
+users = {}
+
+@app.route("/")
+def main():
+    return "commands: /auth, /logout, /send, /getall"
+
+@app.route("/auth")
+def auth():  
+    new_user = request.args.get('name')
+# name !+ '' or None
+    if new_user == '' or new_user == None:
+        return 'Отмена'
+    if users.get(new_user) != None:
+        return 'Такой пользователь уже есть'
+    myuuid = uuid.uuid4()
+    users[new_user]= str(myuuid
+    return str(myuuid)
+
+@app.route("/logout")
+def logout():
+    token = request.args.get('token')
+    for key, value in users.items():
+        if value == token:
+            del users[key]
+            return 'крутаааа вышел'
+    return 'ощепка'
+
+
+@app.route("/send")
+def send():
+    text = request.args.get('text')
+    token = request.args.get('token')
+    print(text,token,users)
+    if text == '':
+        return 'Отмена'
+    if token == '' or token == None:
+        return 'Отмена'
+    
+    for key, value in users.items():
+
+       if value == token:
+            return text
+     
+    return 'err'
+   
+
+@app.route("/getall")
+def getall():
+    x = [
+        {'name': 'Max', 'message': '', 'timestamp':123},
+    ]
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    print('RABOTAEM')
+    {app.run(debug=True)}
